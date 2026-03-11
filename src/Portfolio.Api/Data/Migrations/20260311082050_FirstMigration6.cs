1879ff94e99d5d77c14573cdc913a1c29226327b
dotnetappdev <dotnetappdevni@gmail.com>
2026-03-11 08:33:15 +0000

    fixing config publish
    fixing config publish

---
+++ src/Portfolio.Api/Data/Migrations/20260311082050_FirstMigration6.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BlogPostImageSlots",
+                table: "AppSettings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BlogPostImageSlots",
+                table: "AppSettings",
+                type: "int",
+                nullable: false,
+                defaultValue: 10,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
